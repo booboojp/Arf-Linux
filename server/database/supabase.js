4df31ce284c7371cf1ba45b7e9f072381c80fe95
Jake Schuler <jschuler@hackclub.app>
2025-03-30 15:00:51 -0500

    Fix some supabase prod stuff
    The backend can't communicate with supabase through the reverse proxy so this is my fix.

--- server/database/supabase.js
+++ server/database/supabase.js
@@ -1,11 +1,15 @@
 const { createClient } = require(`@supabase/supabase-js`);
 require(`dotenv`).config();
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY)
+if (!process.env.SUPABASE_URL_INTERNAL || !process.env.SUPABASE_KEY)
     throw new Error(`Missing SUPABASE_URL or SUPABASE_KEY environment variables`);
 
+const SUPABASE_URL = process.env.NODE_ENV === `production`
+    ? process.env.SUPABASE_URL_INTERNAL
+    : process.env.SUPABASE_URL;
+
 const supabase = createClient(
-    process.env.SUPABASE_URL,
+    SUPABASE_URL,
     process.env.SUPABASE_KEY,
     {
         auth: {
@@ -21,4 +25,23 @@ const supabase = createClient(
     }
 );
 
-module.exports = supabase;
+if (process.env.NODE_ENV === `production`) {
+    const externalSupabase = createClient(
+        process.env.SUPABASE_URL_EXTERNAL,
+        process.env.SUPABASE_KEY,
+        {
+            auth: {
+                autoRefreshToken: true,
+                persistSession: true,
+                detectSessionInUrl: true,
+                storage: {
+                    getItem: key => null,
+                    setItem: (key, value) => {},
+                    removeItem: key => {}
+                }
+            }
+        }
+    );
+}
+
+module.exports = supabase, externalSupabase;
